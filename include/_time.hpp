4c9075719abd3512a95b03931e54862258271e8f
Stas Vavich <someonesp@inbox.ru>
2021-12-14 13:16:19 -0800

    0.7.2

--- include/_time.hpp
+++ include/_time.hpp
@@ -6,15 +6,20 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <string>
 
 #include "check.hpp"
+
+using std::string;
+
+using std::vector;
 
 struct Time {
  public:
   void print(std::ostream& os);
   Check_type type;
   std::vector<Check> experiments;
-  Time(Check_type type, std::vector<long>& sizes);
+  Time(Check_type type, std::vector<int64_t>& sizes);
 };
 
 
